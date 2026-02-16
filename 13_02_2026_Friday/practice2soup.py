90d027d0b88b0d6c0183cd67fa3ce3f17ab7fd50
ALokYadav05 <alokak148@gmail.com>
2026-02-16 17:38:51 +0530

    Updated: practice2soup.py

--- 13_02_2026_Friday/practice2soup.py
+++ 13_02_2026_Friday/practice2soup.py
@@ -4,7 +4,7 @@ import requests
 
 url = "https://beautiful-soup-4.readthedocs.io/en/latest/"
 
-headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36'}
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (HTML, like Gecko) Chrome/144.0.0.0 Safari/537.36'}
 
 def basic_config():
     """
