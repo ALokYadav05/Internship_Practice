c5923fd07f92575d333d453c79caae0b2ff251d2
ALokYadav05 <alokak148@gmail.com>
2026-03-31 16:16:01 +0530

    Update: Improved code & added doc-string

--- 31_03_2026_Tuesday/LLM_MemoryMongoDB/main.py
+++ 31_03_2026_Tuesday/LLM_MemoryMongoDB/main.py
@@ -13,7 +13,7 @@ COLLECTION_NAME = "LangChain_context"
 SESSION_ID = 'USER'
 OLLAMA_MODEL = os.getenv("OLLAMA_MODEL")
 
-# langchain mongoDB memory
+# Initialize  mongoDB  to store older chat history permanently
 mongo_memory = MongoDBChatMessageHistory(
                             connection_string=MONGO_URI,
                             database_name=DB_Name,
@@ -22,10 +22,17 @@ mongo_memory = MongoDBChatMessageHistory(
                         )
 
 class ShortTermMemory:
+    """
+    Manages recent messages in RAM and moves old ones to MongoDB when full.
+    """
     def __init__(self,max_size):
         self.memory = deque(maxlen=max_size)
 
     def add_message(self, message):
+        """
+        Adds a new message to short-term memory (RAM).
+        If RAM is full, the oldest message is moved to MongoDB.
+        """
         if len(self.memory) == self.memory.maxlen:
             oldest_msg = self.memory.popleft()
             mongo_memory.add_message(oldest_msg)
@@ -33,10 +40,17 @@ class ShortTermMemory:
         self.memory.append(message)
 
     def get_messages(self):
+        """
+        Returns the current messages stored in RAM as a list
+        :return:
+        """
         return list(self.memory)
 
 # loading context from MongoDB
 def get_long_term_context(limit=4):
+    """
+    Retrieves the last few messages store in MongoDB.
+    """
     messages = mongo_memory.messages
     if not messages:
         return []
@@ -50,24 +64,31 @@ llm = ChatOllama(
 memory = ShortTermMemory(max_size=4)
 
 def chat():
-    print("\n WELCOME TO CHAT (type 'exit' to stop) \n")
+    """
+    Main loop to handle user input and AI responses.
+    """
+    print("\n #### WELCOME TO CHAT (type 'exit' to stop) #### \n")
 
     while True:
         user_input = input("User: ")
-        if user_input == "exit":
+        if user_input.lower() == "exit":
             break
 
+        # Gather history from both RAM and Database
         short_context = memory.get_messages()
         long_context = get_long_term_context()
 
+        # Build the final prompt structure
         system_message = [SystemMessage(content="You are helpful assistant.")]
-        human_message = [HumanMessage(content=user_input)]
+        human_message = HumanMessage(content=user_input)
 
-        final_context = system_message + long_context + short_context + human_message
+        # Combine: System + Old History + Recent History + Current Question
+        final_context = system_message + long_context + short_context + [human_message]
 
         response = llm.invoke(final_context)
         print(f"\n AI: {response.content} \n")
 
+        # Save the exchange to memory
         memory.add_message(human_message)
         memory.add_message(AIMessage(content=response.content))
 
